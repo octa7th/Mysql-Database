48956c0e10228d2fe06a48528b89ed93cd35cc74
Muhammad Sofyan <octa7th@gmail.com>
2013-10-22 10:26:24 +0700

    Add handler to check reserved mysql words to use it as variable

--- database.php
+++ database.php
@@ -682,7 +682,12 @@ class Database
         foreach ($data as $k => $v)
         {
             $keys[] = "`$k`";
-            if($this->setting('prepare'))
+
+            if($this->_check_word($v))
+            {
+                $values[] = $v;
+            }
+            else if($this->setting('prepare'))
             {
                 $values[] = "?";
                 $this->_param_type   .= $this->_determine_type($v);
@@ -705,7 +710,11 @@ class Database
 
         foreach ($data as $k => $v)
         {
-            if($this->setting('prepare'))
+            if($this->_check_word($v))
+            {
+                $changes[] = "`$k` = $v";
+            }
+            else if($this->setting('prepare'))
             {
                 $changes[] = "`$k` = ?";
                 $this->_param_type   .= $this->_determine_type($v);
@@ -949,6 +958,60 @@ class Database
                 return 'd';
                 break;
         }
+    }
+
+    private function _check_word($word)
+    {
+        $words = array(
+            'ACCESSIBLE',         'ADD',                 'ALL',                           'ALTER',             'ANALYZE',
+            'AND',                'AS',                  'ASC',                           'ASENSITIVE',        'BEFORE',
+            'BETWEEN',            'BIGINT',              'BINARY',                        'BLOB',              'BOTH',
+            'BY',                 'CALL',                'CASCADE',                       'CASE',              'CHANGE',
+            'CHAR',               'CHARACTER',           'CHECK',                         'COLLATE',           'COLUMN',
+            'CONDITION',          'CONSTRAINT',          'CONTINUE',                      'CONVERT',           'CREATE',
+            'CROSS',              'CURRENT_DATE',        'CURRENT_TIME',                  'CURRENT_TIMESTAMP', 'CURRENT_USER',
+            'CURSOR',             'DATABASE',            'DATABASES',                     'DAY_HOUR',          'DAY_MICROSECOND',
+            'DAY_MINUTE',         'DAY_SECOND',          'DEC',                           'DECIMAL',           'DECLARE',
+            'DEFAULT',            'DELAYED',             'DELETE',                        'DESC',              'DESCRIBE',
+            'DETERMINISTIC',      'DISTINCT',            'DISTINCTROW',                   'DIV',               'DOUBLE',
+            'DROP',               'DUAL',                'EACH',                          'ELSE',              'ELSEIF',
+            'ENCLOSED',           'ESCAPED',             'EXISTS',                        'EXIT',              'EXPLAIN',
+            'FALSE',              'FETCH',               'FLOAT',                         'FLOAT4',            'FLOAT8',
+            'FOR',                'FORCE',               'FOREIGN',                       'FROM',              'FULLTEXT',
+            'GRANT',              'GROUP',               'HAVING',                        'HIGH_PRIORITY',     'HOUR_MICROSECOND',
+            'HOUR_MINUTE',        'HOUR_SECOND',         'IF',                            'IGNORE',            'IN',
+            'INDEX',              'INFILE',              'INNER',                         'INOUT',             'INSENSITIVE',
+            'INSERT',             'INT',                 'INT1',                          'INT2',              'INT3',
+            'INT4',               'INT8',                'INTEGER',                       'INTERVAL',          'INTO',
+            'IS',                 'ITERATE',             'JOIN',                          'KEY',               'KEYS',
+            'KILL',               'LEADING',             'LEAVE',                         'LEFT',              'LIKE',
+            'LIMIT',              'LINEAR',              'LINES',                         'LOAD',              'LOCALTIME',
+            'LOCALTIMESTAMP',     'LOCK',                'LONG',                          'LONGBLOB',          'LONGTEXT',
+            'LOOP',               'LOW_PRIORITY',        'MASTER_SSL_VERIFY_SERVER_CERT', 'MATCH',             'MAXVALUE',
+            'MEDIUMBLOB',         'MEDIUMINT',           'MEDIUMTEXT',                    'MIDDLEINT',         'MINUTE_MICROSECOND',
+            'MINUTE_SECOND',      'MOD',                 'MODIFIES',                      'NATURAL',           'NOT',
+            'NO_WRITE_TO_BINLOG', 'NULL',                'NUMERIC',                       'ON',                'OPTIMIZE',
+            'OPTION',             'OPTIONALLY',          'OR',                            'ORDER',             'OUT',
+            'OUTER',              'OUTFILE',             'PRECISION',                     'PRIMARY',           'PROCEDURE',
+            'PURGE',              'RANGE',               'READ',                          'READS',             'READ_WRITE',
+            'REAL',               'REFERENCES',          'REGEXP',                        'RELEASE',           'RENAME',
+            'REPEAT',             'REPLACE',             'REQUIRE',                       'RESIGNAL',          'RESTRICT',
+            'RETURN',             'REVOKE',              'RIGHT',                         'RLIKE',             'SCHEMA',
+            'SCHEMAS',            'SECOND_MICROSECOND',  'SELECT',                        'SENSITIVE',         'SEPARATOR',
+            'SET',                'SHOW',                'SIGNAL',                        'SMALLINT',          'SPATIAL',
+            'SPECIFIC',           'SQL',                 'SQLEXCEPTION',                  'SQLSTATE',          'SQLWARNING',
+            'SQL_BIG_RESULT',     'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT',              'SSL',               'STARTING',
+            'STRAIGHT_JOIN',      'TABLE',               'TERMINATED',                    'THEN',              'TINYBLOB',
+            'TINYINT',            'TINYTEXT',            'TO',                            'TRAILING',          'TRIGGER',
+            'TRUE',               'UNDO',                'UNION',                         'UNIQUE',            'UNLOCK',
+            'UNSIGNED',           'UPDATE',              'USAGE',                         'USE',               'USING',
+            'UTC_DATE',           'UTC_TIME',            'UTC_TIMESTAMP',                 'VALUES',            'VARBINARY',
+            'VARCHAR',            'VARCHARACTER',        'VARYING',                       'WHEN',              'WHERE',
+            'WHILE',              'WITH',                'WRITE',                         'XOR',               'YEAR_MONTH',
+            'ZEROFILL'
+        );
+
+        return in_array($word, $words);
     }
 
     /**
