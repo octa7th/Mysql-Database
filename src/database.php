66f8f1cb2a6733274636c5414d384a509454b211
Sofyan <octa7th@gmail.com>
2019-06-18 09:52:13 +0700

    Bump to version 1.5.2

--- src/database.php
+++ src/database.php
@@ -8,7 +8,7 @@
  * @author    Muhammad Sofyan <sofyan@octa7th.com>
  * @copyright 2013 - 2019 Hexastudio
  * @license   http://opensource.org/licenses/MIT
- * @version   1.5.1
+ * @version   1.5.2
  */
 
 class Database
