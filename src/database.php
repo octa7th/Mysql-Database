b2f2f95928766186ee064c3d76a4af2ca9aa5f68
Sofyan <octa7th@gmail.com>
2019-06-18 09:14:27 +0700

    Bump to version 1.5.1

--- src/database.php
+++ src/database.php
@@ -6,9 +6,9 @@
  * @category  Database Access
  * @package   Database
  * @author    Muhammad Sofyan <sofyan@octa7th.com>
- * @copyright 2013 - 2017 Hexastudio
+ * @copyright 2013 - 2019 Hexastudio
  * @license   http://opensource.org/licenses/MIT
- * @version   1.5.0
+ * @version   1.5.1
  */
 
 class Database
